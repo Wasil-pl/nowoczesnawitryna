84bab3ab26d7a458f96db98d51f824bd5cc9e7d2
Dariusz Wasilewski <dariusz.wasilewski.86@gmail.com>
2024-02-24 20:20:37 +0100

    Dodano obsługę ciasteczek do strony

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,16 +1,31 @@
 import Accordion from "./components/Accordion.js";
 import ContactForm from "./components/ContactForm.js";
+import CookiesConsent from "./components/CookieConsent.js";
 import ResponsiveImageManager from "./components/ResponsiveImageManager.js";
 import Scroll from "./components/Scrool.js";
 import showSection from "./components/SectionDisplay.js";
 import TypingWriter from "./components/TypingWriter.js";
-import { myServicePictures } from "./settings.js";
+import { cookieContent, myServicePictures } from "./settings.js";
 
 const app = {
   initContactForm: function () {
     const thisApp = this;
 
     thisApp.contactForm = new ContactForm();
+  },
+
+  initCookiesConsent: function () {
+    const thisApp = this;
+
+    thisApp.cookiesConsent = new CookiesConsent(cookieContent);
+
+    const settingsCookiesButton = document.querySelector(
+      ".settings-cookies-button"
+    );
+
+    settingsCookiesButton.addEventListener("click", function () {
+      thisApp.cookiesConsent.createBanner();
+    });
   },
 
   initTypewriter: function () {
@@ -49,6 +64,7 @@ const app = {
   init: function () {
     const thisApp = this;
 
+    thisApp.initCookiesConsent();
     thisApp.initContactForm();
     thisApp.initTypewriter();
     thisApp.initScroll();
